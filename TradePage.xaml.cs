fac83e4752fe1a5c83c3689fba2384f8b2fcc116
prodUpal <tim.ism06@gmail.com>
2024-10-10 15:12:04 +0500

    цвета скидки не хватает просто

--- TradePage.xaml.cs
+++ TradePage.xaml.cs
@@ -20,10 +20,70 @@ namespace KoloskovTrade
     /// </summary>
     public partial class TradePage : Page
     {
+        int CountInPage = 10;
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Product> CurrentPageList = new List<Product>();
+        List<Product> TableList;
+
         public TradePage()
         {
             InitializeComponent();
             var currentProduct = Koloskov_TradeEntities.GetContext().Product.ToList();
+            TradeListView.ItemsSource = currentProduct;
+
+            TBAllRecords.Text = Koloskov_TradeEntities.GetContext().Product.ToList().Count().ToString();
+            SortBox.SelectedIndex = 0;
+            FiltrBox.SelectedIndex = 0;
+            Update();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void FiltrBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void SortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+
+        public void Update()
+        {
+            var currentProduct = Koloskov_TradeEntities.GetContext().Product.ToList();
+
+            if (SortBox.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.OrderBy(p => p.ProductCost).ToList();
+            }
+            else if (SortBox.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.OrderByDescending(p => p.ProductCost).ToList();
+            }
+
+            if (FiltrBox.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 0 && p.ProductDiscountAmount < 10)).ToList();
+            }
+            else if (FiltrBox.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 10 && p.ProductDiscountAmount < 15)).ToList();
+            }
+            else if (FiltrBox.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => (p.ProductDiscountAmount >= 15)).ToList();
+            }
+
+            currentProduct = currentProduct.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            TBAllRecords.Text = Koloskov_TradeEntities.GetContext().Product.ToList().Count().ToString();
+            TBCount.Text = currentProduct.Count().ToString();
             TradeListView.ItemsSource = currentProduct;
         }
     }
